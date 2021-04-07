bed535674bdf3a96239079bca9a30e8b8a50321e
alsrb4446 <alsrbb4446@naver.com>
2021-04-07 17:24:50 +0900

    Quiz App

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by 신민규 on 2021/03/17.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: [String]
+    let correctAnswer: String
+    
+    init(q: String, a: [String], correctAnswer: String){
+        text = q
+        answer = a
+        self.correctAnswer = correctAnswer
+    }
+}
